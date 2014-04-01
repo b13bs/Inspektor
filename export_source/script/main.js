d71eac95bda02931cbd3cce1dff95be5d9dc791e
gabriel <gabriel.couvrette@gmail.com>
2014-04-01 11:33:38 -0400

    ajout du path sous l'image et lien sur le fichier quand on clique dessus.

--- export_source/script/main.js
+++ export_source/script/main.js
@@ -8,7 +8,7 @@ $(function(){
 		line = line+"<div class='col-md-8 table_header'>Recognised text</div>";
 		line = line+"</div>";
 		
-		line=line+"<img src='file:///"+data[i][0]+"' class='col-md-4'>";
+		line=line+"<div class='col-md-4'><a href='file:///"+data[i][0]+"'><img src='file:///"+data[i][0]+"' class='col-md-12'><p class='col-md-12'>"+data[i][0]+"</p></a></div>";
 		line=line+"<div class='col-md-8'><div class=textbox>"+data[i][1]+"</div>";
 		line=line+"<label>Exactitude: </label>";
 		line=line+"<div class='progress'>";

dcb5439f53da7b723d2705e1753a9fcb1447afce
gabriel <gabriel.couvrette@gmail.com>
2014-04-02 10:28:35 -0400

    fuzzy 2.0

--- export_source/script/main.js
+++ export_source/script/main.js
@@ -112,22 +112,26 @@ function regex_search()
 
 function fuzzy_search()
 {
-	var THRESHOLD = 0.25;
+	var THRESHOLD = 0.6;
 	var filter = $('#filter').val();
-
 	$('.row').each(function() {
 		var text = $(this).find('.textbox').text();
 
-		var a = FuzzySet(text.split(' '));
-		var result = a.get(filter);
 		var similarity = 0;
-		for(i in result)
-		{
-			similarity = Math.max(similarity, result[i][0]);
-		}
+		if(filter.length>0)
+			for(var i=0;i<text.length;i++)
+			{
+				var substring = text.substring(i,Math.min(i+filter.length, text.length));
+				var a = FuzzySet([substring]);
+				console.log(substring);
+				console.log(filter);
+				var result = a.get(filter);
+				console.log(result);
+				if(result != null)
+					similarity = Math.max(similarity, result[0][0]);
+			}
 
-		console.log(similarity);
-		if (similarity >= THRESHOLD)
+		if (similarity >= THRESHOLD || filter.length==0)
 		{
 			$(this).show();
 		}
